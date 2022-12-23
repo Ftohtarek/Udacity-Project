44afab08c3c55bc8f36aa39196baebacd5d89243
unknown <ftohtarek48@gmail.com>
2022-12-23 03:59:54 +0200

    Upload second Project Weather app

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -1,19 +1,54 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+const bodyParser = require('body-parser')
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
-
+app.use(express.static('node_modules'));
 
 // Setup Server
+const host = 8000;
+app.listen(host, () => {
+    console.log('app is listing on host : ', host);
+})
+
+// routing
+
+app.get('/retrive', (req, res) => {
+    res.send(projectData)
+})
+app.post('/add', checkRequest)
+
+function checkRequest(req, res) {
+    let { body } = req
+    console.log('posted Data');
+    console.log(body);
+    if (!body.temperature) {
+        res.send({ isPosted: false });
+        console.log('inValid');
+        return 0
+    }
+    res.send({ isPosted: true })
+    addData(body)
+}
+function addData(req) {
+    projectData.push({
+        temperature: req.temperature,
+        date: req.date,
+        userResponse: req.userResponse
+    })
+}
+
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,5 +1,123 @@
-/* Global Variables */
 
+/* weather Api requirment */
+const url = 'https://api.weatherapi.com/v1/current.json';
+const ApiKey = '?key=65f2bf1ee8ea43ff80a141915222112';
+/* input field Variable*/
+const zipCode = document.getElementById('zip');
+const feelings = document.getElementById('feelings');
+const generateBtn = document.getElementById('generate');
+const entryHolder = document.getElementById('entryHolder')
+let serverData = [];
+/* alert pop-up Ui Element  */
+const popUp =
+{
+    ele: document.getElementById('popUp'),
+    isExisting: () => {
+        return popUp.ele.style.display == 'block' ? true : false
+    },
+    show: (errorMessage) => {
+        let timer = 0
+        popUp.isExisting() ? timer = 4999 : timer = 0;
+        setTimeout(() => {
+            popUp.ele.style.display = 'block';
+            popUp.events(); popUp.ele.innerHTML = errorMessage;
+        }, timer)
+    },
+    hide: () => { popUp.ele.style.display = 'none' },
+    events: () => {
+        popUp.ele.addEventListener('click', () => { popUp.hide() });
+        setTimeout(() => { popUp.hide() }, 5000);
+    },
+
+}
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
+const getTemp = async (url, ApiKey, zipCode) => {
+    /* Async Function that call api to get weather information */
+    const resbonce = await fetch(url + ApiKey + `&q=${zipCode}`, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    try {
+        const data = await resbonce.json()
+        return handeApiError(data)
+    } catch (error) {
+        popUp.show("error in weather Api Request");
+    }
+}
+
+const handeApiError = (data) => {
+    /* function show popup error if there's error coming from api else it return by Location Temperature */
+    if (!data.error) {
+        return data.current.temp_c
+    } else {
+        popUp.show(data.error.message);
+    }
+}
+
+const postDataIntoNodeServer = async (url, data) => {
+    /*send Data to server  */
+    const resbonce = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+    const resbonceData = await resbonce.json();
+    return resbonceData;
+}
+
+const getPostedDataFromServer = async (url) => {
+    /* method get to node  server  to get posted data {projectData} */
+    const resbonce = await fetch(url, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    try {
+        const data = await resbonce.json()
+        return data;
+    } catch (error) {
+        popUp.show('Error in Node Server')
+    }
+}
+
+const pushListRow = () => {
+    /* method push last updata html element into list group  */
+    let obj = serverData[serverData.length - 1]
+    entryHolder.insertAdjacentHTML('beforeend', `
+    <li id="date" class="list-group-item col-4">${obj.date}</li>
+    <li id="temp" class="list-group-item col-4">${obj.temperature}</li>
+    <li id="content" class="list-group-item col-4">${obj.userResponse}</li>
+    `)
+}
+const isZipCodeEmpty = () => {
+    return zipCode.value == '' ? true : false;
+}
+/* Event listener that fire get system functionalty */
+generateBtn.addEventListener('click', function () {
+    /* flexible Promise tree to handel system flow */
+    // stop implemntaion if no value enter
+    if (isZipCodeEmpty()) { popUp.show('Zip Value Is Required'); return 0 }
+    // get data from api 
+    getTemp(url, ApiKey, zipCode.value)
+        .then((temp) => {
+            // post Data to nodejs server
+            postDataIntoNodeServer('http://localhost:8000/add', { date: newDate, temperature: temp, userResponse: feelings.value })
+                .then((res) => {
+                    if (res.isPosted) {
+                        // get all posted Data
+                        getPostedDataFromServer('http://localhost:8000/retrive')
+                            .then(function (data) {
+                                serverData = data;
+                                // updata UI
+                                pushListRow();
+                            })
+                    } else {
+                        popUp.show('No Data Posted To server')
+                    }
+                })
+        })
+})
